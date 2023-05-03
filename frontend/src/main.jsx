134e17a6e3ec8b34f3ede167994198132c50bdc4
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-03 09:33:29 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,8 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./app";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <App />
+);
